01d2dcfaf2e12a8473a4592a0de856ba866fd473
Jan Nox <jan@nox.kiwi>
2026-01-26 16:23:02 +0000

    EUR-4 * Remove unused Controller Method

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -35,38 +35,4 @@ class CategoryController extends AuthenticatedController
         $this->viewBuilder()->setClassName("Json");
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $this->set('success', true);
-        $this->viewBuilder()->setOption('serialize', ['success']);
-        $this->viewBuilder()->setClassName("Json");
-
-        if (!$this->request->is('post')) {
-            $this->set('errors', [
-                'Method is not allowed'
-            ]);
-            return;
-        }
-
-        $data = $this->request->getData();
-        $categoryTable = $this->Category;
-        $entity = $categoryTable->newEmptyEntity();
-        $entity = $categoryTable->patchEntity($entity, $data);
-        $categoryTable->save($entity);
-
-        if ($entity->hasErrors()) {
-            $this->response = $this->response->withStatus(400);
-            $this->set('error', $entity->getErrors());
-            $this->viewBuilder()->setOption('serialize', ['error']);
-            return;
-        }
-        $this->set('category', $entity);
-        $this->viewBuilder()->setOption('serialize', ['success', 'category']);
-        $this->viewBuilder()->setClassName("Json");
-    }
 }
